8d2b2d5cf3832aba6abd6659cdfb6eee1ffbd21f
clau2014 <clau88and@gmail.com>
2024-03-27 01:43:57 -0300

    modificaciones-4

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,19 @@
-////////////////////////Ejerciocio 1////////////////////////
-
-const imageToggle = document.getElementById('changeborder')
+const changeborder = document.getElementById('changeborder')
+const stickerInputs = document.querySelectorAll('input')
+const calculateButton = document.getElementById('count')
+const amount_info = document.getElementById('amount_info')
+const llave_1 = document.getElementById('llave_1')
+const llave_2 = document.getElementById('llave_2')
+const llave_3 = document.getElementById('llave_3')
+const verificar = document.getElementById('verificar')
 
 changeborder.addEventListener('click', () => {
-  if (changeborder.style.border) {
+  if (changeborder.style.border === '2px solid red') {
     changeborder.style.border = ''
   } else {
     changeborder.style.border = '2px solid red'
   }
 })
-
-////////////////////////Ejerciocio 2////////////////////////
-
-const stickerInputs = document.querySelectorAll('input')
-const calculateButton = document.getElementById('count')
-const amount_info = document.getElementById('amount_info')
 
 calculateButton.addEventListener('click', () => {
   let totalStickers = 0
@@ -26,22 +25,12 @@ calculateButton.addEventListener('click', () => {
   if (totalStickers <= 10) {
     amount_info.textContent = `Llevas ${totalStickers} stickers`
   } else {
-    amount_info.textContent = "Llevas demasiados stickers"
+    amount_info.textContent = "Exceso de stickers"
   }
 })
 
-////////////////////////Ejerciocio 3////////////////////////
-
-
-
-const key1 = document.getElementById('key1')
-const key2 = document.getElementById('key2')
-const key3 = document.getElementById('key3')
-const validation = document.getElementById('validation')
-const checker = document.getElementById('checker')
-
-validation.addEventListener('click', () => {
-  const password = `${key1.value}${key2.value}${key3.value}`
+validacion.addEventListener('click', () => {
+  const password = `${llave_1.value}${llave_2.value}${llave_3.value}`
   if (password === '911') {
     checker.textContent = "Password 1 correcto"
   } else if (password === '714') {
